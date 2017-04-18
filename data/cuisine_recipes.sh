83085a1a19eb741efb37d671caecf4762be6d555
rgarcia-herrera <rgarcia@inmegen.gob.mx>
2017-04-17 20:29:04 -0500

    crude count recipes per cuisine

---
+++ data/cuisine_recipes.sh
@@ -0,0 +1,11 @@
+echo " African " ; grep -c African srep00196-s3.csv
+echo " EastAsian " ; grep -c EastAsian srep00196-s3.csv
+echo " EasternEuropean " ; grep -c EasternEuropean srep00196-s3.csv
+echo " LatinAmerican " ; grep -c LatinAmerican srep00196-s3.csv
+echo " MiddleEastern " ; grep -c MiddleEastern srep00196-s3.csv
+echo " NorthAmerican " ; grep -c NorthAmerican srep00196-s3.csv
+echo " NorthernEuropean " ; grep -c NorthernEuropean srep00196-s3.csv
+echo " SouthAsian " ; grep -c SouthAsian srep00196-s3.csv
+echo " SoutheastAsian " ; grep -c SoutheastAsian srep00196-s3.csv
+echo " SouthernEuropean " ; grep -c SouthernEuropean srep00196-s3.csv
+echo " WesternEuropean " ; grep -c WesternEuropean srep00196-s3.csv
