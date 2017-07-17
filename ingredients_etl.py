b213a0fb2642349623c49390293ecb9647c2a02d
rgarcia-herrera <rgarcia@inmegen.gob.mx>
2017-07-17 11:49:48 -0500

    load all ingredients to a sqlite db

---
+++ ingredients_etl.py
@@ -0,0 +1,28 @@
+import csv
+from pony.orm import *
+
+db = Database()
+
+
+class Ingredient(db.Entity):
+    name = Required(str)
+    type = Optional(str)
+
+
+with open('data/srep00196-s3.csv') as f:
+    reader = csv.reader(f)
+    all = set()
+    for r in reader:
+        # discard comments
+        if r[0].startswith('#'):
+            continue
+
+        for ingredient in r[1:]:
+            all.add(ingredient)
+
+db.bind('sqlite', 'ingredients.sqlite', create_db=True)
+db.generate_mapping(create_tables=True)
+
+with db_session:
+    for ingredient in all:
+        Ingredient(name=ingredient)
