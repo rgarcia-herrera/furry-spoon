ef3141942a730184bbc95ffa0f64d8a705987d27
rgarcia-herrera <rgarcia@inmegen.gob.mx>
2017-07-18 15:40:25 -0500

    break down omnivorous network

---
+++ omni2vegan.py
@@ -0,0 +1,32 @@
+import networkx as nx
+from pony.orm import db_session, Database, Required, Json
+
+
+db = Database()
+
+
+class Ingredient(db.Entity):
+    name = Required(str)
+    tags = Required(Json)
+
+
+db.bind('sqlite', 'data/ingredients.sqlite', create_db=False)
+db.generate_mapping(create_tables=False)
+
+g = nx.gpickle.read_gpickle('data/full_nw.pickle')
+
+with db_session:
+    carnic = [i.name for i in Ingredient.select(lambda i:
+                                                'carnic' in i.tags)]
+    animal = [i.name for i in Ingredient.select(lambda i:
+                                                'animal origin' in i.tags)]
+
+vegetarian = g.copy()
+vegetarian.remove_nodes_from(carnic)
+nx.gpickle.write_gpickle(vegetarian, 'data/vegetarian_nw.pickle')
+
+
+vegan = g.copy()
+vegan.remove_nodes_from(carnic)
+vegan.remove_nodes_from(animal)
+nx.gpickle.write_gpickle(vegan, 'data/vegan_nw.pickle')
